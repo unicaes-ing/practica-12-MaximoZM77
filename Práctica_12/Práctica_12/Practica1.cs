5e1f63712f268009f287b4b80e86e50483cf1913
MaximoZM77 <54588599+MaximoZM77@users.noreply.github.com>
2019-11-20 23:37:15 -0600

    Add files via upload

---
+++ Práctica_12/Práctica_12/Practica1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+//Maximo Zamora 15/11/19
+
+namespace Práctica_12
+{
+    class Practica1
+    {
+        [Serializable]
+        public struct Mascota
+        {
+            public string nombre;
+            public string especie;
+            public string raza;
+            public string sexo;
+            public int edad;
+        }
+        static void Main(string[] args)
+        {
+            Mascota mascota1 = new Mascota();
+            FileStream fs;
+            BinaryFormatter formatter = new BinaryFormatter();
+            const string Nombre_Archivo = "Mascota.bin";
+            try
+            {
+                Console.WriteLine("Nombre:");
+                mascota1.nombre = Console.ReadLine();
+                Console.WriteLine("Especie:");
+                mascota1.especie = Console.ReadLine();
+                Console.WriteLine("Raza:");
+                mascota1.raza = Console.ReadLine();
+                Console.WriteLine("Sexo: ");
+                mascota1.sexo = Console.ReadLine();
+                Console.WriteLine("Edad:");
+                mascota1.edad = Convert.ToInt32(Console.ReadLine());
+                fs = new FileStream(Nombre_Archivo, FileMode.Create, FileAccess.Write);
+                formatter.Serialize(fs, mascota1);
+                fs.Close();
+                Console.WriteLine();
+                Console.WriteLine("Su mascota se ha registrado con éxito...");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
---
+++ Práctica_12/Práctica_12/Practica2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+//Maximo Zamora 15/11/19
+
+namespace Práctica_12
+{
+    class Practica2
+    {
+        static void Main(string[] args)
+        {
+            Practica1.Mascota mascota;
+            FileStream fs;
+            BinaryFormatter formatter = new BinaryFormatter();
+            const string Nombre_Archivo = "Mascota.bin";
+            if (File.Exists(Nombre_Archivo))
+            {
+                try
+                {
+                    fs = new FileStream(Nombre_Archivo, FileMode.Open, FileAccess.Read);
+                    mascota = (Practica1.Mascota)formatter.Deserialize(fs);
+                    fs.Close();
+                    Console.WriteLine("Datos de la mascota:\n");
+                    Console.WriteLine("Nombre: {0}",mascota.nombre);
+                    Console.WriteLine("Especie: {0}",mascota.especie);
+                    Console.WriteLine("Raza: {0}",mascota.raza);
+                    Console.WriteLine("Sexo: {0}",mascota.sexo);
+                    Console.WriteLine("Edad: {0} años",mascota.edad);
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
